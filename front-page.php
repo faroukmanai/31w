1da12653ec7dd9b55d39cae038aaad916c760bbf
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-02-16 16:46:16 -0500

    c6 importer md et modifer la page d'accueil

--- front-page.php
+++ front-page.php
@@ -5,20 +5,21 @@
 <main>
     <code>front-page.php</code>
     <h3>index.php</h3>
-<?php 
-    if (have_posts()):
-        while(have_posts()): the_post();
-            // the_title('<h1>','</h1>');
-            // the_permalink();?>
-
-            <h1><a href="<?php the_permalink();?>"><?=get_the_title();?></a></h1>
-<?php        
-            // the_content();
-            // the_excerpt();
-            echo wp_trim_words(get_the_excerpt(),4);
-        endwhile;
-    endif;
-?>
+    <section class="blocflex">
+        <?php 
+            if (have_posts()):
+                while(have_posts()): the_post();
+                    // the_title('<h1>','</h1>');
+                    // the_permalink();?>
+                
+                <article>
+                    <h3><a href="<?php the_permalink();?>"><?=get_the_title();?></a></h3>
+                    <p> <?php  echo wp_trim_words(get_the_excerpt(),4);?></p>
+                </article>
+            <?php endwhile;
+            endif;
+        ?>
+    </section>
 </main>
 <?php get_footer(); ?>
 
