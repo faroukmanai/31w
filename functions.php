0e0a7175e4024e9cea65fdffcce2d9a92663fdf2
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-02-22 11:09:43 -0500

    c7 ajout de fonction qui Modifie la requete principale de Wordpress

--- functions.php
+++ functions.php
@@ -29,4 +29,22 @@ function enregistrement_des_menus(){
         ) );
     }
 add_action( 'after_setup_theme', 'enregistrement_des_menus', 0 );
+
+/**
+ * Modifie la requete principale de Wordpress avant qu'elle soit exécuté
+ * le hook « pre_get_posts » se manifeste juste avant d'exécuter la requête principal
+ * Dépendant de la condition initiale on peut filtrer un type particulier de requête
+ * Dans ce cas çi nous filtrons la requête de la page d'accueil
+ * @param WP_query  $query la requête principal de WP
+ */
+function cidweb_modifie_requete_principal( $query ) {
+    if ( $query->is_home() // si page d'accueil
+        && $query->is_main_query() // si requête principale
+        && ! is_admin() ) {// non tableau de bord
+      $query->set( 'category_name', 'note-wp' );// filtre les articles de catégorie "note-wp"
+      $query->set( 'orderby', 'title' );// trie selon le titre
+      $query->set( 'order', 'ASC' );//en prdre ascendant
+      }
+     }
+     add_action( 'pre_get_posts', 'cidweb_modifie_requete_principal' );
                     
