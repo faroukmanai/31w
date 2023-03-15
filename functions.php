8cd3161bbbb3f56705da1ae54ae61984fa452258
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-03-15 09:52:54 -0400

    c13 modifier menu cours pour adaptatif

--- functions.php
+++ functions.php
@@ -55,12 +55,27 @@ function cidweb_modifie_requete_principal( $query ) {
 * @param $args : Objet qui représente la structure de menu
 * @param $depth : Niveau des sous menu
 */
-function perso_menu_item_title($title, $item, $args, $depth) {
-    // Remplacer 'nom_de_votre_menu' par l'identifiant de votre menu
-    if($args->menu == 'cours') {
-    // Modifier la longueur du titre en fonction de vos besoins
-    $title = wp_trim_words($title, 3, ' ... ');
-    }
-    return $title;// À ameliorer
-    }
-    add_filter('nav_menu_item_title', 'perso_menu_item_title', 10, 4);
+// function perso_menu_item_title($title, $item, $args, $depth) {
+//     // Remplacer 'nom_de_votre_menu' par l'identifiant de votre menu
+//     if($args->menu == 'cours') {
+//     // Modifier la longueur du titre en fonction de vos besoins
+//     $title = wp_trim_words($title, 3, ' ... ');
+//     }
+//     return $title;// À ameliorer
+//     }
+//     add_filter('nav_menu_item_title', 'perso_menu_item_title', 10, 4);
+
+
+    function perso_menu_item_title($title, $item, $args, $depth) {
+        // Remplacer 'nom_de_votre_menu' par l'identifiant de votre menu
+        if($args->menu == 'cours') {
+        // Modifier la longueur du titre en fonction de vos besoins
+        $sigle = substr($title,4,3);
+        $title = substr($title,7);
+        $title = "<div class='cours__sigle'>" . $sigle ."</div>". 
+                "<p class='cours__titre'>". $title . wp_trim_words($title, 2, ' ... ') . "</p>";
+        
+        }
+        return $title;
+        }
+        add_filter('nav_menu_item_title', 'perso_menu_item_title', 10, 3);
