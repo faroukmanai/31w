6e0a139d4bdf64076371a7c3c6e5297b96ae6bd4
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-03-16 14:59:18 -0400

    c14 menu entete debut theme

--- functions.php
+++ functions.php
@@ -66,16 +66,16 @@ function cidweb_modifie_requete_principal( $query ) {
 //     add_filter('nav_menu_item_title', 'perso_menu_item_title', 10, 4);
 
 
-    function perso_menu_item_title($title, $item, $args, $depth) {
-        // Remplacer 'nom_de_votre_menu' par l'identifiant de votre menu
-        if($args->menu == 'cours') {
-        // Modifier la longueur du titre en fonction de vos besoins
-        $sigle = substr($title,4,3);
-        $title = substr($title,7);
-        $title = "<div class='cours__sigle'>" . $sigle ."</div>". 
-                "<p class='cours__titre'>". $title . wp_trim_words($title, 2, ' ... ') . "</p>";
-        
-        }
-        return $title;
-        }
-        add_filter('nav_menu_item_title', 'perso_menu_item_title', 10, 3);
+function perso_menu_item_title($title, $item, $args) {
+    // Remplacer 'nom_de_votre_menu' par l'identifiant de votre menu  
+  
+    if($args->menu == 'cours') {
+    // Modifier la longueur du titre en fonction de vos besoins
+    $sigle = substr($title, 4,3);
+    $title = substr($title, 7);
+    $title = "<div class='cours__sigle'>" . $sigle . "</div>" . 
+              "<p class='cours__titre'>" . wp_trim_words($title, 2, ' ... ') . "</p>";
+    }
+    return  $title ;
+}
+add_filter('nav_menu_item_title', 'perso_menu_item_title', 10, 3);
--- header.php
+++ header.php
@@ -11,11 +11,15 @@
     <header class="site__entete">
         <section class="logomenu">
             <?php the_custom_logo(); ?>
+            <input type="checkbox" id="chkBurger">
             <?php wp_nav_menu(array(
                 'menu'=>'entete',
                 'container'=>'nav'
             ));?>
             <?= get_search_form(); ?>
+            <label for="chkBurger">
+            <img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="32" height="32">
+            </label>
         </section>
         <h1><a href="<?php bloginfo('url');?>"><?php bloginfo('name');?></a></h1>
         <h2><?php bloginfo('description');?></h2>
