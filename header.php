da4b35a6f6da4d2c141bcf3bb9682c1182088a61
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-03-16 16:49:02 -0400

    c14 adapter le menu pour mobile, tablette et desktop

--- header.php
+++ header.php
@@ -5,29 +5,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php wp_head() ?>
+    <?php wp_head(); ?>
 </head>
-<body class="site <?= (is_front_page()? "no_aside":"")?>">
-    <header class="site__entete">
+<body class="site <?= (is_front_page() ? "no-aside" : "");  ?> ">
+    <header class="site__entete">  
         <section class="logomenu">
-            <?php the_custom_logo(); ?>
-            <input type="checkbox" id="chkBurger">
-            <?php wp_nav_menu(array(
-                'menu'=>'entete',
-                'container'=>'nav'
-            ));?>
-            <?= get_search_form(); ?>
-            <label for="chkBurger">
-            <img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="32" height="32">
-            </label>
+            <?php the_custom_logo(); ?> 
+            <div class="menusearch">
+                <input type="checkbox" id="chkBurger">
+                <?php wp_nav_menu(array(
+                    'menu' => 'entete',
+                    'container' => 'nav'
+                )); ?> 
+                <?= get_search_form(); ?>
+                <label for="chkBurger" class="burger">
+                <img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="32" height="32">
+                </label>   
+            </div> 
         </section>
-        <h1><a href="<?php bloginfo('url');?>"><?php bloginfo('name');?></a></h1>
-        <h2><?php bloginfo('description');?></h2>
-       
+        <h1><a href="<?php  bloginfo('url'); ?>"><?php  bloginfo('name'); ?></a></h1> 
+        <h2><?php  bloginfo('description'); ?></h2>
     </header>
-    <?php
-    if (!is_front_page())
+    <?php 
+    if (is_front_page() == false)
     {
-        get_template_part("template-parts/aside");
+       get_template_part("template-parts/aside");
     }
-    ?>
+     ?>
+    
