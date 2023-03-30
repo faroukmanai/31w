8f75af30c544db1ea3db6fad08249882a3f52783
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-03-30 16:31:59 -0400

    c18 integration d'un evenement dans front-page

--- front-page.php
+++ front-page.php
@@ -4,6 +4,16 @@
 <?php get_header(); ?>
 <main class="site__main">
     <!-- <code>front-page.php</code> -->
+    <section class="viewport">
+        <h2>Tp2</h2>
+        <h3>Auteur: Farouk Manai</h3>
+        <h4>Cours: 31w - Introduction à un gestionnaire de contenu</h4>
+        <?php wp_nav_menu(array(
+            "menu"=>"evenement",
+            "container"=>"nav",
+            "container_class"=>"menu__bloc"
+        )); ?>
+    </section>
     <!-- <h3>index.php</h3> -->
     <section class="blocflex">
         <?php 
