9dbb930a5104ec46f62671d9d1751e5837b8f992
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-04-06 21:44:56 -0400

    Ajustement du lieu de l'evenement dans template-evenement.php

--- template-evenement.php
+++ template-evenement.php
@@ -12,7 +12,7 @@ if ( have_posts() ) : the_post(); ?>
 <?php the_post_thumbnail('thumbnail');?>
 <h1><?= get_the_title(); ?></h1>
 <?php the_content();?>
-<p>L'adresse de l'evénement<?php the_field('adresse'); ?></p>
+<p>L'adresse de l'evénement<?php the_field("lieu_de_l'evenement"); ?></p>
 <p>La date et l'heure de l'événement <?php the_field('date_de_levenement'); ?></p>     
 <?php endif;?>
 </main><!-- #main -->
