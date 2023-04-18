7a2781e7c5fe1b3097108b649d6aa93b50c6ad2d
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-04-18 15:04:25 -0400

    EF3 integration du search form dans la page 404

--- 404.php
+++ 404.php
@@ -4,8 +4,9 @@
 <?php get_header(); ?>
 <main class="site__main">
     <!-- <code>404.php</code> -->
-    <h1>Erreur 400 </h1>
-    <h3>Page introuvable vous pouvez tenter une recherche</h3>
+    <h1>Erreur 404</h1>
+    <h3>Page introuvable. Vous pouvez tenter une recherche :</h3>
+    <?php get_search_form(); ?>
 </main>
 <?php get_footer(); ?>
 
